d95ae6ed5fbda3338f28a3054c93eb6e3875fd9f
g4urav <gaurav.gb.bahuguna@gmail.com>
2023-07-16 10:51:14 +0530

    slider inmprovement+ tasklisst updated

--- components/Task2.js
+++ components/Task2.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Instruction = () => {
   return (
     <div className='bg-white dark:bg-gray-900'>
-   
+     
       <div className="px-4 pt-16">
         <div className="max-w-5xl mx-auto bg-white rounded-lg shadow-lg p-8">
           <div className='pb-10'>
@@ -42,12 +42,45 @@ const Instruction = () => {
               <img className="w-3/5 rounded-lg shadow-md" src="/images/clients/after.png" alt="After image example" />
             </div>
           </div>
-          <div className="flex justify-end mt-10">
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-10 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Next</button>
+
+          <h1 className='text-black text-center font-bold text-3xl mt-10'>TASK SUBMISSION</h1>
+          <div className=" ">
+            {/* <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-10 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Next</button> */}
+
+            <div class="flex justify-center mt-8">            
+              <div class="rounded-lg shadow-xl bg-gray-50 lg:w-1/2">
+                <div class="m-4">
+                  <label class="inline-block mb-2 text-gray-500">Upload
+                    Image(jpg,png,svg,jpeg)</label>
+                  <div class="flex items-center justify-center w-full">
+                    <label class="flex flex-col w-full h-32 border-4 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                      <div class="flex flex-col items-center justify-center pt-7">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                          class="w-12 h-12 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
+                          fill="currentColor">
+                          <path fill-rule="evenodd"
+                            d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                            clip-rule="evenodd" />
+                        </svg>
+                        <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                          Select a photo</p>
+                      </div>
+                      <input type="file" class="opacity-0" />
+                    </label>
+                  </div>
+                </div>
+                <div class="flex p-2 space-x-4">
+                 
+                  <button class="px-4 py-2 text-white bg-red-500 rounded shadow-xl">Cannel</button>
+                  <button class="px-4 py-2 text-white bg-green-500 rounded shadow-xl">Upload</button>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
- 
+   
     </div>
   );
 };
--- components/Track.js
+++ components/Track.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const StyledTrack=styled.div`
 
 transition: transform 300ms cubic-bezier(0.42,0,0.58,1) 0s;
-transform: translate3d(${(p) => 23 * -p.current}vw,0,0);
+ transform: translate3d(${props => (props.current) ? `${-90}vw` : '0vw'}, 0, 0);
 @media (max-width: 767px) {
   
 transition: transform 300ms cubic-bezier(0.42,0,0.58,1) 0s;
