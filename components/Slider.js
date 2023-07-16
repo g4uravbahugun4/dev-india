388f6e6d3305c38346507a7ad0b0b10a96b29df8
darkillusior <mysteryuniverse12@gmail.com>
2023-07-16 11:09:23 +0530

    fo

--- components/Slider.js
+++ components/Slider.js
@@ -14,7 +14,7 @@ function Slider({slides,current,setCurrent}) {
         
           <li onClick={()=>{setSelected(index)}}
          className={`${selected === index ? ' p-4 min-w-full  max-w-full  px-4 md:min-w-[50%] xl:min-w-[25%] md:max-w-[50%] xl:max-w-[25%] m-5 transition-all ease-out duration-300 scale-125' :'p-4 min-w-full max-w-full  px-4 md:min-w-[33%] xl:min-w-[25%] sm:max-w-[33%]  xl:max-w-[25%] m-5 cursor-pointer opacity-30  transition-all ease-out duration-300 hover:scale-110'} `}>
-            {item} 
+          <Card/>
           </li>
           ))
           } 
